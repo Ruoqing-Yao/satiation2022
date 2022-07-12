e4562982eda5a7d6a0105d9ab81b4539741b7298
Ruoqing Yao <ryao03@email.wm.edu>
2022-07-12 16:16:59 -0700

    finished randamization of stimuli

--- experiment/exp2/js/new_exp.js
+++ experiment/exp2/js/new_exp.js
@@ -20,16 +20,76 @@ var shuffle = function (array) {
 	}
 
 	return array;
-
 };
 
-// experiment parameters
 
-num_test_items = 12; // how many target items in the exposure phase?
-num_g_items = 10; // how many target items in the test phase?
-num_unique_lex_items = num_exposure_items + num_test_items;
-num_fillers = 11; /* half the number of target items. Each type of filler
-                  will have this many items */
-block_size = 4;
-num_exposure_blocks = num_exposure_items/2; // 2 target items per block
-num_test_blocks = num_test_items/2; // 2 target items per block
+
+num_target_items = 12;
+const num_gram_filler = 12;
+num_ungram_filler = 12;
+block_size = 3;
+
+experiment_condition = shuffle(["WH", "CNPC"])[0];
+experiment_group = shuffle(["RP", "GAP"])[0];
+
+
+console.log("condition", experiment_condition);
+console.log("group", experiment_group);
+
+/**
+ * 
+ * @param {*} items 
+ * @param {*} group filter 1
+ * @param {*} cond  filter 2
+ * @returns filtered items
+ * 
+ * Method Name: Filt
+ * Purpose: filter an array with two filter
+ * 
+ */
+
+/* 
+*Method Name: Filt
+Purpose: filter an array with two filter
+Paramter: group, cond (two filtering conditions)
+*/
+function filt(items, condition, num){
+    return items.filter(item => (item.condition == condition /*&& item.condition == cond*/
+                        && item.lex_items <= num))
+}
+
+//Desired stimuli list
+experiment_stimuli = filt(all_stimuli, experiment_condition, 12);
+//Desired grammatical filler
+gram_filler = filt(all_stimuli, "FILL", 112);
+//Desired ungrammatical filler
+ungram_filler = filt(all_stimuli, "UNGRAM", 128);
+
+
+//Testing logs
+console.log("stimuli ", experiment_stimuli);
+console.log("filler ", gram_filler);
+console.log("ungram ", ungram_filler);
+
+
+/**
+ * Pseudo-block randomization of three lists of testing items
+ * @param {*} list1 three lists of items
+ * @param {*} list2 
+ * @param {*} list3 
+ * @returns An array of randomized items
+ */
+function pseudo_block(list1, list2, list3){
+    output = [];
+    num = l1.length;
+    for(var i = 0; i < num; i++){
+        block = [list1.pop(), list2.pop(), list3.pop()];
+        block = shuffle(block);
+        output.push(block);
+    }
+    return output;
+}
+
+final_stimuli = pseudo_block(experiment_stimuli, gram_filler, ungram_filler);
+console.log("final stimuli", final_stimuli);
+
